2acc260531e274a8b75e8611d0d69b387968093d
daniellreyes <daniell.reyes26@gmail.com>
2023-12-25 12:58:20 -0600

    create endpoints as crud

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ app = FastAPI()
 @app.get('/')
 def welcome():
     return {'message': 'Welcome to my FastAPI API'}
+
+@app.get('/api/tasks')
+async def get_tasks():
+    return 'all tasks'
+
+@app.post('/api/tasks')
+async def create_tasks():
+    return 'create tasks'
+
+@app.get('/api/tasks/{id}')
+async def get_task():
+    return 'single task'
+
+@app.put('/api/tasks/{id}')
+async def update_tasks():
+    return 'updating tasks'
+
+@app.delete('/api/tasks/{id}')
+async def delete_task():
+    return 'delete task'
+
